94861b1d567013da0caf2ed4110028d07250ef3c
Calum Bain <43376276+Calum532@users.noreply.github.com>
2019-01-28 13:56:57 +0000

    Timer functionality added

---
+++ Ninja Game/Assets/Scripts/Player/Timer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    private float startTime;
+
+    private void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f0");
+
+        timerText.text = minutes + ":" + seconds;
+    }
+}
